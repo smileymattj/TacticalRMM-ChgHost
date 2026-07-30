21abae7ebf50697040f98fe49c7221236788dcb8
smileymattj <61148969+smileymattj@users.noreply.github.com>
2026-07-30 15:14:06 -0400

    Update chgClientAgent.sh
    Update Linux Client Agent

--- chgClientAgent.sh
+++ chgClientAgent.sh
@@ -6,5 +6,4 @@ NewURL="example.xyz"
 sed -i.bak "s/$OldURL/$NewURL/g" /etc/tacticalagent
 sed -i.bak "s/$OldURL/$NewURL/" /opt/tacticalmesh/meshagent.msh
 
-systemctl restart meshagent
-systemctl restart tacticalagent
+systemctl restart meshagent tacticalagent
