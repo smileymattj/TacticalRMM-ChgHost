5fe856d753c89802a7f8caa2e2fa92183a042e2f
smileymattj <61148969+smileymattj@users.noreply.github.com>
2026-07-30 15:15:52 -0400

    Update chgClientAgent.ps1
    Update Windows Client Agent

--- chgClientAgent.ps1
+++ chgClientAgent.ps1
@@ -5,9 +5,9 @@ $MeshPath = "C:\Program Files\Mesh Agent\MeshAgent.msh"
 
 Stop-Service -Name $RMMServices
 
-((Get-Content -Raw -Path $MeshPath) -replace $OldURL,$NewURL) | Set-Content -Path $MeshPath
-
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\TacticalRMM /f /v ApiURL /d api.$NewURL
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\TacticalRMM /f /v BaseURL /d https://api.$NewURL
+
+((Get-Content -Raw -Path $MeshPath) -replace $OldURL,$NewURL) | Set-Content -Path $MeshPath
 
 Start-Service -Name $RMMServices
