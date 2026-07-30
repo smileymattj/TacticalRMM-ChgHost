a1fd718497ac54b2d4d36ee54061979a461cdc18
smileymattj <61148969+smileymattj@users.noreply.github.com>
2026-07-30 16:30:43 -0400

    Update chgClientAgent.ps1

--- chgClientAgent.ps1
+++ chgClientAgent.ps1
@@ -3,11 +3,9 @@ $NewURL = "example.xyz"
 $RMMServices = ("Mesh Agent", "tacticalrmm")
 $MeshPath = "C:\Program Files\Mesh Agent\MeshAgent.msh"
 
-Stop-Service -Name $RMMServices
-
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\TacticalRMM /f /v ApiURL /d api.$NewURL
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\TacticalRMM /f /v BaseURL /d https://api.$NewURL
 
 ((Get-Content -Raw -Path $MeshPath) -replace $OldURL,$NewURL) | Set-Content -Path $MeshPath
 
-Start-Service -Name $RMMServices
+Restart-Service -Name $RMMServices
